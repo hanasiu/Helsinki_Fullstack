f6008f9020147aaf40f913c64d0c485a518ddbe6
hanasiu <102237333+hanasiu@users.noreply.github.com>
2022-12-09 13:59:49 +0900

    Create 1.9: App.js

---
+++ Part 1 Introduction to React/1.9: App.js
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+  )
+
+const Display = (props) => <div>{props.text} {props.value}</div>
+
+const DisplayPositive = (props) => <div>{props.text} {props.value} %</div>
+
+const Statistics = ({good, neutral, bad}) => {
+  const count = good + neutral + bad;
+  if(count === 0) return (<div><b>No feedback given</b></div>) 
+  return (
+    <div>
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+      <Display text="all" value={good+neutral+bad} />
+      <Display text="average" value={(good*1+neutral*0+bad*-1)/(good+neutral+bad)} />
+      <DisplayPositive text="positive" value={(good/(good+neutral+bad))*100} />
+    </div>
+  )
+}
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const plusGood = (newValue) => {
+    setGood(newValue);
+  } 
+
+  const plusNeutral = (newValue) => {
+    setNeutral(newValue);
+  } 
+
+  const plusBad = (newValue) => {
+    setBad(newValue);
+  } 
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => plusGood(good+1)} text="good" />
+      <Button handleClick={() => plusNeutral(neutral+1)} text="neutral" />
+      <Button handleClick={() => plusBad(bad+1)} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
