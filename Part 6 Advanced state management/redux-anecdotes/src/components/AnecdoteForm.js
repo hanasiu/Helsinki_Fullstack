9aaa7ef2a0108598917abad08a52f5cd0b2165ae
hanasiyou <youryouu@naver.com>
2023-03-03 15:07:52 +0900

    part 6

---
+++ Part 6 Advanced state management/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,24 @@
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification} from '../reducers/notificationReducer'
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch()
+
+    const addAnecdote = async (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        dispatch(createAnecdote(content))
+        dispatch(setNotification(`you created ${content} `, 5))
+    }
+
+    return (
+        <form onSubmit={addAnecdote}>
+            <input name="anecdote" />
+            <button type="submit">add</button>
+        </form>
+    )
+}
+
+export default AnecdoteForm
